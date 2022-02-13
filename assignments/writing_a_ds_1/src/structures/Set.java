70787f4f099656faf715313f89d82228d666b74e
Josh Archer <jarcher@greenriver.edu>
2022-02-13 13:32:32 -0800

    Added Node class implementations to begin with.

--- assignments/writing_a_ds_1/src/structures/Set.java
+++ assignments/writing_a_ds_1/src/structures/Set.java
@@ -3,4 +3,32 @@ package structures;
 public class Set
 {
 
+    private class Node
+    {
+        //the Object type should be changed to your
+        //generic type
+        private Object data;
+        private Node prev;
+        private Node next;
+
+        public Node(Object data)
+        {
+            this.data = data;
+        }
+
+        public Node(Object data, Node prev, Node next)
+        {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        @Override
+        public String toString()
+        {
+            String prev = this.prev != null ? this.prev.data.toString() : "null";
+            String next = this.prev != null ? this.prev.data.toString() : "null";
+            return prev + " <-- " + data.toString() + " --> " + next;
+        }
+    }
 }
